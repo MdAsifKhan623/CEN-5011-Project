56104ce3814373cb84fd3e7227fe9bc65224332b
mariojacas <mjaca001@cs.fiu.edu>
2020-10-20 19:05:50 -0400

    upload dev-mario branch

--- flaskbackend/main.py
+++ flaskbackend/main.py
@@ -1,10 +1,1 @@
-from flask import Flask
-
-app=Flask(__name__)
-
-@app.route("/home", methods=['GET'])
-def main():
-    return {
-        "uid":1,
-        'title':"coviddashboard"
-    }
+from app import app
