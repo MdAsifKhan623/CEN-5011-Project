1b8ec37f626e40bf6e20684ddeef2522371999ee
mariojacas <mjaca001@cs.fiu.edu>
2020-10-20 21:05:11 -0400

    added API call

--- flaskbackend/app/mod_home/controllers.py
+++ flaskbackend/app/mod_home/controllers.py
@@ -1,9 +1,22 @@
 from app import app
 from flask import Blueprint, redirect, url_for, abort, render_template, render_template_string
+import requests
 
 mod_home = Blueprint('home', __name__)
 
 @mod_home.route('/home', methods=['GET'])
 def index():
-	a = { "uid":1, 'title':"coviddashboard"}
+	url = "https://api.covid19api.com/country/south-africa/status/confirmed"
+    payload = {}
+    headers= {}
+    d = requests.request("GET", url, headers=headers, data = payload)
+    Country = []
+    Date = []
+    Count = []
+    for dates in d.json():
+        Country.append(dates['Country'])
+        #Date.append(dates[''])
+        Count.append(dates['Cases'])
+
+	a = { "uid":1, 'title':"coviddashboard", "total":sum(Count)}
 	return a
