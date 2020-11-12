1b42526401481a03232b3be16ce964106212176d
Asif623 <mdasifk22@gmail.com>
2020-11-11 20:23:28 -0500

    Country Section works fine

--- src/views/countryComponents/countries.js
+++ src/views/countryComponents/countries.js
@@ -1,40 +1,86 @@
 import React,{useState, useEffect} from 'react'
-import TotalCard from '../totalCard'
-import {Container, Row, Col} from 'react-bootstrap'
+import TabHeader from '../tabHeader'
+import {Container, Row, Col, Form, Button} from 'react-bootstrap'
 import axios from 'axios'
+import {ImageCard,DiffCard} from '../totalCard'
+import TotalCard from '../totalCard'
 
 export default function Countries(){
+    let covidPositiveCases, covidDeathCases,covidRecoveryCases,casesStatus, perMillionCard,testCard
     const [formvalue,setValue]=useState('')
     const [apiCountryData,setCountryData]=useState({})
-
+    const [countryFlag,setCountryFlag]=useState('https://disease.sh/assets/img/flags/us.png')
     const handleChange=(e)=>{
         setValue(e.target.value)
     }
-
+    const fetchUSData=(e)=>{
+        axios.get('https://disease.sh/v3/covid-19/countries/United%20States?yesterday=true&strict=true')
+        .then((res)=>{
+            setCountryData(res.data)
+        })
+    }
+    useEffect(()=>{
+        fetchUSData()
+    },[])
     const handleSubmit=(e)=>{
         // const url="https://disease.sh/v3/covid-19/countries/"+{formvalue}+"?strict=true"
         // console.log(url)
         axios.get(`https://disease.sh/v3/covid-19/countries/${formvalue}?yesterday=true&strict=true`)
         .then((res)=>{
             setCountryData(res.data)
-            
+            setCountryFlag(res.data.countryInfo.flag)
         })
         console.log(apiCountryData)
         e.preventDefault();
         
     }
+    if (Object.keys(apiCountryData).length !== 0){
+        covidPositiveCases= <TotalCard title="Positive Cases" content={apiCountryData.cases} posVal={apiCountryData.todayCases}/>
+        covidDeathCases=<TotalCard title="Death Cases" content={apiCountryData.deaths} posVal={apiCountryData.todayDeaths}/>
+        covidRecoveryCases=<TotalCard title="Recovery Cases" content={apiCountryData.recovered} posVal={apiCountryData.todayRecovered}/>
+        casesStatus=<DiffCard title="Cases Status" name1="Active:" cont1={apiCountryData.active} name2="Critical:" cont2={apiCountryData.critical}/>
+        perMillionCard= <DiffCard title="Per Million Cases Counts" name2="Cases/Million:" name1="Deaths/Million:" cont1={apiCountryData.casesPerOneMillion} cont2={apiCountryData.deathsPerOneMillion}/>
+        testCard=<DiffCard title="Test Counts" name2="Test/Million:" name1="Tests:" cont1={apiCountryData.tests} cont2={apiCountryData.testsPerOneMillion}/>
+    }
     
     return (
         <div>
         <Container fluid>
-        <form onSubmit={handleSubmit}>
-                <label>
-                    Enter the Country Name:
-                    <input type="text" value={formvalue} onChange={handleChange} />
-                </label>
-                <input type="submit" value="Submit" />
+        <center><TabHeader title="Country Name"/></center>
+        <Form onSubmit={handleSubmit}>
+            <Form.Group controlId="formBasicPassword">
+                <br/>
+                <center>
+                    <Form.Control className="country-field" onChange={handleChange} type="text" placeholder="Enter Country Name" />
+                </center>
+            </Form.Group>
+            <center>
+                <Button variant="primary" type="submit">
+                    Submit
+                </Button>
+            </center>
+            
+        </Form>
+        {console.log(apiCountryData)}
+        <br/>
+        <center>
+
+        <ImageCard name={apiCountryData.country} continent={apiCountryData.continent} image={countryFlag}/>
         
-            </form>
+        </center>
+        <br/>
+        <Row>
+            <Col xs="12" md='4'>{covidPositiveCases}</Col>
+            <Col xs="12" md='4'>{covidDeathCases}</Col>
+            <Col xs="12" md='4'>{covidRecoveryCases}</Col>
+        </Row>
+        <br/>
+        <Row>
+            <Col xs="12" md='4'>{casesStatus}</Col>
+            <Col xs="12" md='4'>{perMillionCard}</Col>
+            <Col xs="12" md='4'>{testCard}</Col>
+        </Row>
+        <br/>
         </Container>
 
         </div>
--- src/views/totalCard.js
+++ src/views/totalCard.js
@@ -14,3 +14,37 @@ export default function TotalCard(props){
             </Card>
     </div>)
 }
+
+function ImageCard(props){
+    return(<div>
+        <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src={props.image} />
+            <Card.Body>
+                <Card.Title>{props.name}</Card.Title>
+                <Card.Text>
+                {props.continent}
+                </Card.Text>
+            </Card.Body>
+        </Card>
+    </div>)
+}
+
+function DiffCard(props){
+    return (
+        <div>
+            <Card className="card-pos-death" style={{  }}>
+                <Card.Body>
+                    <Card.Title><h2>{props.title}</h2></Card.Title>
+                    {/* <Card.Subtitle className="mb-2 text-muted">{props.posVal}</Card.Subtitle> */}
+                    <Card.Text className="covid-counts">
+                    {props.name1} {props.cont1}
+                    </Card.Text>
+                    <Card.Text className="covid-counts">
+                    {props.name2} {props.cont2}
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+        </div>
+    )
+}
+export {ImageCard,DiffCard}
