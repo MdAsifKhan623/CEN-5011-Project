070acd515c968d3ab1c1253ce17ef0737fad8fb0
Asif623 <mdasifk22@gmail.com>
2020-11-15 23:16:12 -0500

    removed the unrequired imports

--- src/views/ContinentComponents/ContinentMapData.js
+++ src/views/ContinentComponents/ContinentMapData.js
@@ -3,15 +3,15 @@ import ReactFC from 'react-fusioncharts';
 import FusionCharts from 'fusioncharts';
 import FusionMapsAsia from 'fusionmaps/maps/es/fusioncharts.asia';
 import Asia from 'fusioncharts/fusioncharts.maps'
-import Europe from 'fusioncharts/fusioncharts.maps'
-import NorthAmerica from 'fusioncharts/fusioncharts.maps'
-import SouthAmerica from 'fusioncharts/fusioncharts.maps'
-import Australia from 'fusioncharts/fusioncharts.maps'
+// import Europe from 'fusioncharts/fusioncharts.maps'
+// import NorthAmerica from 'fusioncharts/fusioncharts.maps'
+// import SouthAmerica from 'fusioncharts/fusioncharts.maps'
+// import Australia from 'fusioncharts/fusioncharts.maps'
 import FusionTheme from 'fusioncharts/themes/fusioncharts.theme.fusion';
-import FusionMapsEurope from 'fusionmaps/maps/es/fusioncharts.europe'
-import FusionMapsNorthAmerica from 'fusionmaps/maps/es/fusioncharts.northamerica'
-import FusionMapsSouthAmerica from 'fusionmaps/maps/es/fusioncharts.southamerica'
-import FusionMapsAustralia from 'fusionmaps/maps/es/fusioncharts.australia'
+// import FusionMapsEurope from 'fusionmaps/maps/es/fusioncharts.europe'
+// import FusionMapsNorthAmerica from 'fusionmaps/maps/es/fusioncharts.northamerica'
+// import FusionMapsSouthAmerica from 'fusionmaps/maps/es/fusioncharts.southamerica'
+// import FusionMapsAustralia from 'fusionmaps/maps/es/fusioncharts.australia'
 
 ReactFC.fcRoot(FusionCharts, FusionMapsAsia, Asia, FusionTheme);
 
