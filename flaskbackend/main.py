ab162b4b80a9c8e71060f9eb107ff1fa2a3f1ca4
Asif623 <mdasifk22@gmail.com>
2020-11-27 20:37:36 -0500

    vaccine and news section works

--- flaskbackend/main.py
+++ flaskbackend/main.py
@@ -1,10 +1,29 @@
 from flask import Flask
+import boto3
+import requests
 
 app=Flask(__name__)
+
+# dynamoDB=boto3.resource('dynamodb')
+# dynamoTable=dynamoDB.Table('TestingCenters_Table')
+
+# dynamoTable.put_item(
+#     Item={
+#         'id':"oklahoma",
+#         'data':result.json()
+#     }
+# )
+# @app.route('/site',methods=['POST'])
+# def stateSites():
+
+
 @app.route("/home", methods=['GET'])
 def main():
     return {
-        "uid":1,
-        'title':"covid dashboard"
+        'id':"1",
+        'data':result.json()
     }
+@app.route("/testing-site", methods=['GET'])
+def testingSite():
+    
 from app import app
